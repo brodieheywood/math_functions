9f7bb88703f86c85aaf84f46fab79f53a579b1c2
Brodie Heywood <brodieheywood@gmail.com>
2019-01-31 23:57:19 -0800

    Create test_eratosthenes.py

---
+++ test_eratosthenes.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from comp_1510_a2 import q01
+
+
+class TestEratosthenes(TestCase):
+    def test_eratosthenes_general_case(self):
+        self.assertEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29], q01.eratosthenes(30))
+
+    def test_eratosthenes_smallest_case(self):
+        self.assertEqual([2], q01.eratosthenes(3))
+
+    def test_eratosthenes_smallest_case_with_no_result(self):
+        self.assertEqual([], q01.eratosthenes(1))
+
+    def test_eratosthenes_zero(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes(0)
+
+    def test_eratosthenes_negative(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes(-1)
+
+    def test_eratosthenes_float(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes(10.5)
+
+    def test_eratosthenes_negative_float(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes(-10.5)
+
+    def test_eratosthenes_empty(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes()
+
+    def test_eratosthenes_string(self):
+        with self.assertRaises(TypeError):
+            q01.eratosthenes('a')
